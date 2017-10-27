e8d241968e115084af5268b6f95dca56664504a2
Briar Bowser <briarbear7@gmail.com>
2017-10-27 10:57:35 -0500

    Added support for custom icons.
    Addded custom previous, next, and destroy icon support to android.
    
    Renamed 'destroyIcon' to 'closeIcon' to be consistent.
    Added 'notificationIcon'.
    Uses 'notificationIcon' instead of play/pause if provided.
    Update readme to document these new additions.
    
    Re-worked icon documentation.

--- www/MusicControls.js
+++ www/MusicControls.js
@@ -1,12 +1,12 @@
 var musicControls = {
-  updateCallback: function () {},
+  updateCallback: function() {},
 
-  create: function (data, successCallback, errorCallback) {
-    data.artist = !isUndefined(data.artist) ? data.artist : '';
-    data.track = !isUndefined(data.track) ? data.track : '';
-    data.album = !isUndefined(data.album) ? data.album : '';
-    data.cover = !isUndefined(data.cover) ? data.cover : '';
-    data.ticker = !isUndefined(data.ticker) ? data.ticker : '';
+  create: function(data, successCallback, errorCallback) {
+    data.artist = !isUndefined(data.artist) ? data.artist : "";
+    data.track = !isUndefined(data.track) ? data.track : "";
+    data.album = !isUndefined(data.album) ? data.album : "";
+    data.cover = !isUndefined(data.cover) ? data.cover : "";
+    data.ticker = !isUndefined(data.ticker) ? data.ticker : "";
     data.duration = !isUndefined(data.duration) ? data.duration : 0;
     data.elapsed = !isUndefined(data.elapsed) ? data.elapsed : 0;
     data.isPlaying = !isUndefined(data.isPlaying) ? data.isPlaying : true;
@@ -19,42 +19,85 @@ var musicControls = {
     data.skipBackwardInterval = !isUndefined(data.skipBackwardInterval) ? data.skipBackwardInterval : 0;
     data.hasClose = !isUndefined(data.hasClose) ? data.hasClose : false;
     data.dismissable = !isUndefined(data.dismissable) ? data.dismissable : false;
+    data.playIcon = !isUndefined(data.playIcon) ? data.playIcon : "";
+    data.pauseIcon = !isUndefined(data.pauseIcon) ? data.pauseIcon : "";
+    data.prevIcon = !isUndefined(data.prevIcon) ? data.prevIcon : "";
+    data.nextIcon = !isUndefined(data.nextIcon) ? data.nextIcon : "";
+    data.closeIcon = !isUndefined(data.closeIcon) ? data.closeIcon : "";
+    data.notificationIcon = !isUndefined(data.notificationIcon) ? data.notificationIcon : "";
 
-    cordova.exec(successCallback, errorCallback, 'MusicControls', 'create', [data]);
-  },
-
-  updateIsPlaying: function (isPlaying, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'MusicControls', 'updateIsPlaying', [{isPlaying: isPlaying}]);
-  },
-  updateElapsed: function (args, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'MusicControls', 'updateElapsed', [{
-      elapsed: args.elapsed,
-      isPlaying: (args.isPlaying === undefined) ? '' : !!args.isPlaying
-    }]);
-  },
-  updateDismissable: function (dismissable, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'MusicControls', 'updateDismissable', [{dismissable: dismissable}]);
+    cordova.exec(successCallback, errorCallback, "MusicControls", "create", [
+      data
+    ]);
   },
 
-  destroy: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'MusicControls', 'destroy', []);
+  updateIsPlaying: function(isPlaying, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "MusicControls",
+      "updateIsPlaying",
+      [{ isPlaying: isPlaying }]
+    );
+  },
+  updateElapsed: function(args, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "MusicControls",
+      "updateElapsed",
+      [
+        {
+          elapsed: args.elapsed,
+          isPlaying: args.isPlaying === undefined ? "" : !!args.isPlaying
+        }
+      ]
+    );
+  },
+  updateDismissable: function(dismissable, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "MusicControls",
+      "updateDismissable",
+      [{ dismissable: dismissable }]
+    );
+  },
+
+  destroy: function(successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "MusicControls",
+      "destroy",
+      []
+    );
   },
 
   // Register callback
-  subscribe: function (onUpdate) {
+  subscribe: function(onUpdate) {
     musicControls.updateCallback = onUpdate;
   },
   // Start listening for events
-  listen: function () {
-    cordova.exec(musicControls.receiveCallbackFromNative, function (res) {
-    }, 'MusicControls', 'watch', []);
+  listen: function() {
+    cordova.exec(
+      musicControls.receiveCallbackFromNative,
+      function(res) {},
+      "MusicControls",
+      "watch",
+      []
+    );
   },
-  receiveCallbackFromNative: function (messageFromNative) {
+  receiveCallbackFromNative: function(messageFromNative) {
     musicControls.updateCallback(messageFromNative);
-    cordova.exec(musicControls.receiveCallbackFromNative, function (res) {
-    }, 'MusicControls', 'watch', []);
+    cordova.exec(
+      musicControls.receiveCallbackFromNative,
+      function(res) {},
+      "MusicControls",
+      "watch",
+      []
+    );
   }
-
 };
 
 function isUndefined(val) {
